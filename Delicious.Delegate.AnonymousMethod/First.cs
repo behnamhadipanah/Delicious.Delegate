68db10280e863249192b833adc11c21bf129e15e
behnamhadipanah <behnamhadipanah@gmail.com>
2025-09-01 11:46:34 -0700

    update type of delegate

---
+++ Delicious.Delegate.AnonymousMethod/First.cs
@@ -0,0 +1,38 @@
+﻿namespace Delicious.Delegate.AnonymousMethod;
+
+public class First
+{
+    delegate int MyDelegate(int a, int b);
+    delegate void MyDelegate2();
+
+    public void Run()
+    {
+        MyDelegate dlg = delegate(int a, int b)
+        {
+            return a + b;
+        };
+        
+        int result=dlg(100, 20);
+        Console.WriteLine($"dlg value is : {result}");
+        
+        //MyDelegate dlg2=(x,y)=>x*y;
+        MyDelegate dlg2=(x,y)=>
+        {
+            x = x + x;
+            return x * y;
+        };
+        result=dlg2(100, 20);
+        Console.WriteLine($"dlg2 value is : {result}");
+
+
+        //MyDelegate2 dlg3 = ()=>Console.WriteLine("dlg3");
+        MyDelegate2 dlg3 = ()=>
+        {
+            string name = "testing";
+            Console.WriteLine($"dlg3 {name}");
+        };
+        dlg3();
+        
+        Console.ReadKey();
+    }
+}
---
+++ Delicious.Delegate.Generic/First.cs
@@ -0,0 +1,56 @@
+﻿namespace Delicious.Delegate.Generic;
+
+public class First
+{
+    delegate TResult MyDelegate<T1,T2,TResult>(T1 arg1, T2 arg2);
+    
+    
+
+    public void Run()
+    {
+        MyDelegate<int, double, double> dlg1 = delegate(int a, double b)
+        {
+            return a / b;
+        };
+        double result=dlg1(10,2);
+
+        Console.WriteLine($"result is {result}");
+        Console.WriteLine("******************************************");
+        //default delegate microsoft
+        
+        //Func with result 
+       Func<int, double, double> dlg2 = delegate(int a, double b)
+        {
+            return a / b;
+        };
+       // Func<int, double, double> dlg2 =(a,b)=> a / b;
+         result=dlg2(10,2);
+        Console.WriteLine($"result is {result}");
+        Console.WriteLine("******************************************");
+        
+        // Func without parameter
+        Func<string> dlg3 = ()=>"without parameter";
+        
+        //Action without result
+        
+        Action<string> dlg4 = x=>Console.WriteLine("default delegate without result ");
+        dlg4("action");
+        
+        //Predicate with one parameter and result is boolean
+       // Predicate<int> dlg5 = x => x % 2 == 0;
+       //Func<int,bool> dlg5 = PredicateMethod;
+       //bool predicateDelegate=dlg5(10);
+        
+       Predicate<int> dlg5 = x => x % 2 == 0;
+        bool predicateDelegate=dlg5(10);
+        Console.WriteLine($"predicateDelegate  is {predicateDelegate}");
+        
+        Console.ReadKey();
+
+    }
+
+    bool PredicateMethod(int param)
+    {
+        return param % 2 == 0;
+    }
+}
